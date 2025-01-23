6c1207a9203422af631e2aa3528afc3a066e3058
viraxpress <vira@viraxpress.com>
2025-01-23 09:35:33 +0000

    COntroller update for session end

--- Controller/Checkout/Index.php
+++ Controller/Checkout/Index.php
@@ -24,35 +24,62 @@ declare(strict_types=1);
 
 namespace ViraXpress\CheckoutOptimization\Controller\Checkout;
 
-use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\Controller\ResultInterface;
-use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
 
-class Index implements HttpGetActionInterface
+class Index extends \Magento\Checkout\Controller\Onepage implements HttpGetActionInterface
 {
-
     /**
-     * @var PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
-     * Constructor
+     * Checkout page
      *
-     * @param PageFactory $resultPageFactory
+     * @return \Magento\Framework\Controller\ResultInterface
      */
-    public function __construct(PageFactory $resultPageFactory)
+    public function execute()
     {
-        $this->resultPageFactory = $resultPageFactory;
+        /** @var \Magento\Checkout\Helper\Data $checkoutHelper */
+        $checkoutHelper = $this->_objectManager->get(\Magento\Checkout\Helper\Data::class);
+        if (!$checkoutHelper->canOnepageCheckout()) {
+            $this->messageManager->addErrorMessage(__('One-page checkout is turned off.'));
+            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+        }
+
+        $quote = $this->getOnepage()->getQuote();
+        if (!$quote->hasItems() || $quote->getHasError() || !$quote->validateMinimumAmount()) {
+            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+        }
+
+        if (!$this->_customerSession->isLoggedIn() && !$checkoutHelper->isAllowedGuestCheckout($quote)) {
+            $this->messageManager->addErrorMessage(__('Guest checkout is disabled.'));
+            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+        }
+
+        // generate session ID only if connection is unsecure according to issues in session_regenerate_id function.
+        // @see http://php.net/manual/en/function.session-regenerate-id.php
+        if (!$this->isSecureRequest()) {
+            $this->_customerSession->regenerateId();
+        }
+        $this->_objectManager->get(\Magento\Checkout\Model\Session::class)->setCartWasUpdated(false);
+        $this->getOnepage()->initCheckout();
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
     }
 
     /**
-     * Execute view action
+     * Checks if current request uses SSL and referer also is secure.
      *
-     * @return ResultInterface
+     * @return bool
      */
-    public function execute(): ResultInterface
+    private function isSecureRequest(): bool
     {
-        return $this->resultPageFactory->create();
+        $request = $this->getRequest();
+
+        $referrer = $request->getHeader('referer');
+        $secure = false;
+
+        if ($referrer) {
+            $scheme = parse_url($referrer, PHP_URL_SCHEME);
+            $secure = $scheme === 'https';
+        }
+
+        return $secure && $request->isSecure();
     }
 }
